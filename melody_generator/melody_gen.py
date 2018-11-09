4d84d635cad81e7d196d9dc5796aa6935d05cbdf
0SNP0 <niksel2002@gmail.com>
2018-11-09 12:31:16 +0700

    модель есть

---
+++ melody_generator/melody_gen.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+train_in = np.array([[1.0, 6.0, 5.0, 0.5, 1.0, 0.1]])
+train_out = np.array([3.0])
+test_in, test_out = train_in, train_out
+
+model = keras.Sequential([
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(7, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_in, train_out, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_in, test_out)
+print('Test accuracy:', test_acc)
